385cdeed36d2e4ab94a993eb4b9a006c5660973f
grecabo123 <artamay1@gmail.com>
2022-02-28 00:32:27 +0800

    updated 80%

--- Home/admin/incident.php
+++
@@ -1,7 +0,0 @@
-<?php  
-
-	include '../connector/connect.php';
-	$query = "SELECT Incident_Type, count(*) as total from incident_report GROUP BY Incident_Type";
-	$result_i = mysqli_query($conn,$query);
-	
-?>
--- Home/personnel/chief_relation/js/script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-	$('#report').click(function(event) {
-		$('.dropdown').toggleClass('show');
-		$('#caret').toggleClass('rotate');
-	});
-});
